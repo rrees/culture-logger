57025e223414d135e6948d366a7b177223379cf8
Robert Rees <shudderfix@gmail.com>
2016-03-07 09:17:17 +0000

    Creates the mapping function from the model to a JSON format for the client

--- api.py
+++ api.py
@@ -5,12 +5,21 @@ from google.appengine.api import users
 
 import queries
 
+def log_to_json(log):
+	return {
+		"name": log.name,
+		"date": log.date.isoformat(),
+		"tags": log.tags,
+		"rating": log.rating,
+		"notes": log.notes,
+	}
+
 class LogsForYear(webapp2.RequestHandler):
 	def get(self, year):
 		user = users.get_current_user()
 		logs = queries.period.year(user, int(year))
 
-		self.response.write(json.dumps([l for l in logs]))
+		self.response.write(json.dumps([log_to_json(l) for l in logs]))
 
 
 app = webapp2.WSGIApplication([

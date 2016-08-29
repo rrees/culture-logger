e43e16f18d2c20f03b8840695d737b818e54c471
Robert Rees <shudderfix@gmail.com>
2016-08-29 23:10:17 +0100

    Uses a filter to replace the number with unicode stars

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import jinja2
 
 import queries
 import constants
+import filters
 
 from google.appengine.api import users
 
@@ -14,6 +15,8 @@ JINJA = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
+
+JINJA.filters.update(filters.custom_filters)
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
